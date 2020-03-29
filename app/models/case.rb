923cc76dad736483619129213d4514f1b7a7b2c3
Ankur Patel <ankur@encoredevlabs.com>
2020-03-29 14:25:04 -0400

    Fixing saving of cases

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,7 +1,7 @@
 class Case < ApplicationRecord
   belongs_to :city
 
-  default_scope { order(date: :desc) }
+  default_scope { order(date: :desc, created_at: :desc) }
 
-  validates_uniqueness_of :date
+  validates_uniqueness_of :date, scope: :city_id
 end
